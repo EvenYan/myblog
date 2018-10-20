18852e9fcccf3d3082f3dc2089ea74e1c69c422d
even <EvenYan>
2018-10-20 12:03:17 +0800

    视图函数

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,9 @@ from blog import views
 
 urlpatterns = [
     url(r"addpost", views.add_post, name="addpost"),
+    url(r"^$", views.index, name="index"),
     url(r"posts", views.get_post_list, name="posts"),
-    url(r"detail/(\d+)", views.detail, name="detail"),
+    url(r"detail/(?P<num>\d+)", views.detail, name="detail"),
+    url(r"info", views.get_form, name="info"),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,41 @@ from __future__ import unicode_literals
 import random
 
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from django.urls import reverse
+
 from blog.models import Post
+
+
+def index(request):
+    print("request path:", request.path)
+    print("request method:", request.method)
+    print("request encoding:", request.encoding)
+    print("request Cookies:", request.COOKIES)
+    print("request get:", request.GET)
+    # request.GET['name']
+    # request.GET.get('name', "陌生人")
+    print("request post:", request.POST)
+    print("request get host:", request.get_host())
+
+    username = request.POST.get("username")
+    passwd = request.POST.get("passwd")
+    print("用户名", username)
+    print("密码:", passwd)
+
+    response = HttpResponse()
+    response.content = "你好，这是content定义的内容"
+    response.status_code = 302
+    response.write("这是write方法写入的数据")
+    response.set_cookie('name', 'alice', max_age=30)
+
+    return redirect(reverse('blog:detail', kwargs={"num": 10}))
+
+
+def get_form(requset):
+    return render(requset, 'blog/form.html')
 
 
 def get_post_list(request):
@@ -26,7 +57,8 @@ def add_post(request):
     return HttpResponse("Hello Django!")
 
 
-def detail(request, id):
-    post = get_object_or_404(Post, id=id)
+def detail(request, num):
+    # a = 10 / 0
+    post = get_object_or_404(Post, id=num)
     context = {"post": post}
     return render(request, 'blog/detail.htm', context=context)
